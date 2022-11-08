623a2e93dd29b0e9248ce38ced23978f2d8d08a8
Dario Reis <guttyerresjunior1995@gmail.com>
2022-11-08 20:40:59 -0300

    adiciona Projeto

---
+++ src/pages/Home/index.js
@@ -0,0 +1,30 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Dario Reis</h1>
+      <span>Veja meus links 👇</span>
+
+      <main className="links">
+        <section className="link-area">
+          <a href="#">
+            <p className="link-text">Canal no Youtube</p>
+          </a>
+        </section>
+
+        <section className="link-area">
+          <a href="#">
+            <p className="link-text">Canal no Youtube</p>
+          </a>
+        </section>
+
+        <section className="link-area">
+          <a href="#">
+            <p className="link-text">Canal no Youtube</p>
+          </a>
+        </section>
+      </main>
+    </div>
+  );
+}
