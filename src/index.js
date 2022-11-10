9b7dd1230554cba1baac399a186f09ca27951354
Dario Reis <guttyerresjunior1995@gmail.com>
2022-11-10 13:51:42 -0300

    adiciona autenticação e alertas personalizados

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { router } from "./App";
 import "./index.css";
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+
 import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ToastContainer/>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,21 +2,59 @@ import { useState } from "react";
 import "./login.css";
 import { Logo } from "./../../components/Logo/index";
 
+import { autenticacao } from "../../services/firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+import { toast } from "react-toastify";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
 
-  function handleLogin(event){
-    event.preventDefault(); //Para previnir que a página atualize 
-    alert("Teste")
+  function handleLogin(event) {
+    event.preventDefault(); //Para previnir que a página atualize
+
+    if (email === "" || password === "") {
+      alert("preencha todos os campos!");
+      return;
+    }
+
+    signInWithEmailAndPassword(autenticacao, email, password)
+      .then(() => {
+        navigate("/admin", { replace: true });
+        toast.success("Bem Vindo de volta", {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      })
+      .catch(() => {
+        toast.error("Erro ao tentar fazer seu login", {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      });
   }
 
   return (
     <div className="login-container">
       <Logo />
 
-      <form className="form" onSubmit={handleLogin} >
+      <form className="form" onSubmit={handleLogin}>
         <input
           type="email"
           placeholder="Digite seu email..."
